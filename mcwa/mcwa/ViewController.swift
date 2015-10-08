9fca607241976f148d79dc37d1ff2616269244d7
xingfuqiu <xingfuqiu@gmail.com>
2015-10-08 23:01:50 +0800

    add files

--- mcwa/mcwa/ViewController.swift
+++ mcwa/mcwa/ViewController.swift
@@ -13,12 +13,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.title = "MC哇!"
+        
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        
+        let background = turquoiseColor()
+        background.frame = self.view.bounds
+        self.view.layer.insertSublayer(background, atIndex: 0)
+        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //颜色渐变
+    func turquoiseColor() -> CAGradientLayer {
+        
+        let topColor = UIColor(hexString: "#362057")
+        
+        let bottomColor = UIColor(hexString: "#3b3f73")
+        
+        let gradientColors: Array <AnyObject> = [topColor!.CGColor, bottomColor!.CGColor]
+        
+        let gradientLocations: Array <NSNumber> = [0.0, 1.0]
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        
+        gradientLayer.colors = gradientColors
+        
+        gradientLayer.locations = gradientLocations
+        
+        return gradientLayer
+        
+    }
+
 
 
 }
