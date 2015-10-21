278196eb0fabe09e0a14f7b1d13ea6c2de07e7d9
xingfuqiu <xingfuqiu@gmail.com>
2015-10-21 23:39:27 +0800

    完善贡献列表

---
+++ mcwa/mcwa/contributionListCell.swift
@@ -0,0 +1,47 @@
+//
+//  contributionListCell.swift
+//  mcwa
+//
+//  Created by XingfuQiu on 15/10/21.
+//  Copyright © 2015年 XingfuQiu. All rights reserved.
+//
+
+import UIKit
+
+class contributionListCell: UITableViewCell {
+
+    @IBOutlet weak var lb_title: UILabel!
+    @IBOutlet weak var lb_answerCount: UILabel!
+    @IBOutlet weak var lb_accuracy: UILabel!
+    @IBOutlet weak var lb_time: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func update(json: JSON) {
+        //        println(json)
+        
+        lb_title.text = json["title"].stringValue
+        let accuracy = json["rightCount"].floatValue / json["allCount"].floatValue
+        lb_accuracy.text = "正确率:\(accuracy)"
+//        let dt = json["insertTime"].stringValue
+
+        lb_time.text = "强仔服务器处理"
+        //let st = json["status"].stringValue
+        //if st == "pass" then
+        
+        lb_answerCount.text = json["allCount"].stringValue
+
+    }
+
+
+}
--- mcwa/mcwa/contributionListController.swift
+++ mcwa/mcwa/contributionListController.swift
@@ -10,18 +10,69 @@ import UIKit
 
 class contributionListController: UITableViewController {
 
+    let cellIdentifier = "contributionListCell"
+    var isFirstLoad = true
+    var manager = AFHTTPRequestOperationManager()
+    var page: PageInfo!
+    var json: JSON! {
+        didSet {
+            if "ok" == self.json["state"].stringValue {
+                //分页信息
+                page = PageInfo(j: self.json["dataObject", "pageBean"])
+                //数据
+                if let d = self.json["dataObject", "data"].array {
+                    if page.currentPage == 1 {
+                        //                        println("刷新数据")
+                        self.datasource = d
+                    } else {
+                        //                        println("加载更多")
+                        self.datasource = self.datasource + d
+                    }
+                }
+            }
+        }
+    }
+    
+    var datasource: Array<JSON>! = Array() {
+        didSet {
+            if self.datasource.count < page.allCount {
+                self.tableView.footer.hidden = self.datasource.count < page.pageSize
+                print("没有达到最大值 \(self.tableView.footer.hidden)")
+            } else {
+                print("最大值了,noMoreData")
+                self.tableView.footer.endRefreshingWithNoMoreData()
+            }
+            
+            self.tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "贡献列表"
         let barButtonItem = UIBarButtonItem()
         barButtonItem.title = ""
         self.navigationItem.backBarButtonItem = barButtonItem
+        
+        self.tableView.header = MJRefreshNormalHeader(refreshingBlock: {self.loadNewData()})
+        self.tableView.footer = MJRefreshAutoNormalFooter(refreshingBlock: {self.loadMoreData()})
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        if isFirstLoad {
+            loadDataWithoutMJRefresh()
+        }
     }
+    
+    func loadDataWithoutMJRefresh() {
+        //        hud = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        //        hud?.labelText = MCUtils.TEXT_LOADING
+        loadNewData()
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -30,20 +81,68 @@ class contributionListController: UITableViewController {
 
     // MARK: - Table view data source
     
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        return 1
-//    }
+    func loadNewData() {
+        //开始刷新
+        //http://221.237.152.39:8081/interface.do?act=uploadList&userId=2&page=1
+        let dict = ["act":"uploadList", "userId": 2, "page": 1]
+        manager.GET(URL_MC,
+            parameters: dict,
+            success: { (operation: AFHTTPRequestOperation!,
+                responseObject: AnyObject!) in
+                //println(responseObject)
+                self.isFirstLoad = false
+                self.json = JSON(responseObject)
+                self.tableView.header.endRefreshing()
+                //                self.hud?.hide(true)
+            },
+            failure: { (operation: AFHTTPRequestOperation!,
+                error: NSError!) in
+                //                println("Error: " + error.localizedDescription)
+                self.tableView.header.endRefreshing()
+                //                self.hud?.hide(true)
+                MCUtils.showCustomHUD("数据加载失败", aType: .Error)
+        })
+    }
+    
+    func loadMoreData() {
+        //        println("开始加载\(self.page.currentPage+1)页")
+        let dict = ["act":"uploadList", "userId": 2, "page": page.currentPage+1]
+        //println("加载:\(self.liveType),\(self.liveOrder)======")
+        //开始刷新
+        manager.GET(URL_MC,
+            parameters: dict,
+            success: { (operation: AFHTTPRequestOperation!,
+                responseObject: AnyObject!) in
+                //println(responseObject)
+                self.json = JSON(responseObject)
+                self.tableView.footer.endRefreshing()
+            },
+            failure: { (operation: AFHTTPRequestOperation!,
+                error: NSError!) in
+                //                println("Error: " + error.localizedDescription)
+                self.tableView.footer.endRefreshing()
+                MCUtils.showCustomHUD("数据加载失败", aType: .Error)
+        })
+    }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 10
+        if (!self.datasource.isEmpty) {
+            //            self.tableView.backgroundView = nil
+            return self.datasource.count
+        } else {
+            //            MCUtils.showEmptyView(self.tableView, aImg: Load_Empty!, aText: "什么也没有,下拉刷新试试?")
+            return 0
+        }
     }
     
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("contributionListCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! contributionListCell
+        
+        let j = self.datasource[indexPath.row] as JSON
+        cell.update(j)
 
-        // Configure the cell...
 
         return cell
     }
